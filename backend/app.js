e38b1e5769bdfb275484646b2e3b77f87225f9b4
PiedraL <joaquinlangone12@gmail.com>
2024-08-01 02:07:57 -0300

    Creating endpoints. Working in backend

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cars = require('./db/cars.json')
-
+const crypto = require('node:crypto')
+const { validateCar, validatePartialCar } = require('./schemas/cars')
 // const carsRoutes = require('./routes/cars')
 
 const app = express()
@@ -11,10 +12,110 @@ const PORT = process.env.PORT ?? 3456
 
 // app.use('/cars', carsRoutes)
 
-app.get('/' , (req, res) => {
+app.get('/cars/:id' , (req, res) => {
   res.header('Access-Control-Allow-Origin', '*')
 
+  const { id } = req.params // Get ID from the request param
+  const car = cars.find( car => car.id === parseInt(id))
+  if ( car ) return res.json( car )
+
+  res.status(404).json({ message: "Car not found" })
+})
+
+app.get('/cars' , (req , res) => {
+
+  res.header('Access-Control-Allow-Origin', '*')
+
+  const { year, chasis, brand, model, condition } = req.query
+  if( year ) {
+    const filterByYear = cars.filter(
+      car => parseInt(car.year) === parseInt(year)
+    )
+    return res.json(filterByYear)
+  }
+
+  if( chasis ) {
+    const filterByChasis = cars.filter(
+      car => car.chasis.toLowerCase() === chasis.toLowerCase()
+    )
+    return res.json(filterByChasis)
+  }
+
+  if( brand ) {
+    const filterByBrand = cars.filter(
+      car => car.brand.toLowerCase() === brand.toLowerCase()
+    )
+    return res.json(filterByBrand)
+  }
+
+  if( model ) {
+    const filterByModel = cars.filter(
+      car => car.model.toLowerCase() === model.toLowerCase()
+    )
+    return res.json(filterByModel)
+  }
+
+  if ( condition ) {
+    const filterByCondition = cars.filter(
+      car => car.condition.toLowerCase() === condition.toLowerCase()
+    )
+    return res.json(filterByCondition)
+  }
+
+
   res.json(cars)
+
+})
+
+
+
+app.post('/cars' , (req, res) => {
+
+  const result = validateCar(req.body) // we send the body request to our validation schema
+  if (!result.success) {
+    return res.status(400).json({ error : JSON.parse(result.error.message) })
+  }
+
+  const newCar = {
+    id: crypto.randomUUID(),
+    ...result.data
+  }
+
+  // cars.push(newCar) // push to the database
+  res.status(201).json(newCar)
+})
+
+
+
+app.patch('/cars/:id' , (req, res) => {
+
+  const result = validatePartialCar(req.body)
+  if(!result.success) {
+    return res.status(400).json({ error : JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const carIndex = cars.findIndex( car => car.id == id) // fix later
+  if ( carIndex === -1 ) {
+    return res.status(404).json({ message: "Car not found" })
+  }
+
+  const updateCar = {
+    ...cars[carIndex],
+    ...result.data
+  }
+
+  cars[carIndex] = updateCar
+
+  return res.json(updateCar)
+})
+
+app.options('/cars/:id', (req, res) => {
+    // This is for CORS, because delete is a complex methos needs this options method
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+
+    res.send(200)
 })
 
 
---
+++ backend/schemas/cars.js
@@ -0,0 +1,36 @@
+const { z } = require('zod')
+
+// Zod schema for car validation
+
+const carSchema = z.object({
+  chasis: z.enum(['pickup', 'sedan', 'coupe', 'suv']),
+  brand: z.string(),
+  model: z.string(),
+  year: z.number().min(1800).max(new Date().getFullYear() + 2),
+  price: z.number().nonnegative(),
+  color: z.array(z.string()).min(1).max(5),
+  condition: z.enum(['used', 'brand new']),
+  datasheet: z.object({
+    hp: z.number().nonnegative(),
+    engine: z.string(),
+    fuel: z.enum(['gasoline', 'diesel']),
+    consumption: z.string(),
+    torque: z.string(),
+    max_speed: z.string(),
+    weight: z.string(),
+    kilometers: z.number().nonnegative(),
+    features: z.array(z.string()),
+    drivetrain: z.string(),
+    transmission: z.string(),
+  }),
+})
+
+function validateCar ( input ) {
+  return carSchema.safeParse( input ) // safeParse will return an object that will say if there is an error or if there is data
+}
+
+function validatePartialCar ( input ) {
+  return carSchema.partial().safeParse( input ) // Partial for if we want to modify a part of the car's info.
+}
+
+module.exports = { validateCar, validatePartialCar }
