3305dc6c5953dd7006bfb42dfd555738da4f06a4
PiedraL <joaquinlangone12@gmail.com>
2024-07-31 23:20:46 -0300

    Working in the backend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cars = require('./db/cars.json')
 
-const carsRoutes = require('./routes/cars')
+// const carsRoutes = require('./routes/cars')
 
 const app = express()
 app.disable('x-powered-by')
@@ -9,7 +9,14 @@ app.use(express.json())
 
 const PORT = process.env.PORT ?? 3456
 
-app.use('/cars', carsRoutes)
+// app.use('/cars', carsRoutes)
+
+app.get('/' , (req, res) => {
+  res.header('Access-Control-Allow-Origin', '*')
+
+  res.json(cars)
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
